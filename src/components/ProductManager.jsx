2604bf25e5aae17d37273569f4549dd0d0419a1f
Matheus-Lino <maheuslinoeua@gmail.com>
2025-11-03 22:23:31 -0300

    Mudança no packejson

--- src/components/ProductManager.jsx
+++ src/components/ProductManager.jsx
@@ -146,7 +146,7 @@ import {
 } from '@chakra-ui/react';
 import { EditIcon, DeleteIcon, AddIcon } from '@chakra-ui/icons';
 
-const API_BASE_URL = 'http://127.0.0.1:5000'; 
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 
 function ProductManager() {
